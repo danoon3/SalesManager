81745365bb0415350ca2f2d1bdf4fd38fb4e4916
Denis Chernikov <danoone58@gmail.com>
2022-12-20 21:56:43 +0300

    Added class Main and README.md

--- Lesson2.1/src/Main.java
+++ Lesson2.1/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SalesManager salesManager = new SalesManager(new int[]{100, 250, 800, 600});
+
+        System.out.println(salesManager.max());
     }
 }
