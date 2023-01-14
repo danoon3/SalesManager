a7a13a59d796ec2c199e1fe6d3526343e6cb4594
Denis Chernikov <danoone58@gmail.com>
2023-01-14 14:20:39 +0300

    Added statistic

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         SalesManager salesManager = new SalesManager(new int[]{100, 250, 800, 600});
 
         System.out.println(salesManager.max());
+        System.out.println("Усеченное среднее значение из списка продаж равно " + salesManager.statistic());
     }
 }
