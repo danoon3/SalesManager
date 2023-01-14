2938de32c4e8b76932b8131b8408dc968c68f601
Denis Chernikov <danoone58@gmail.com>
2023-01-14 14:59:48 +0300

    Some changes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        SalesManager salesManager = new SalesManager(new long[]{100, 250, 800, 600});
+        SalesManager salesManager = new SalesManager(new int[]{100, 250, 800, 600});
 
         System.out.println(salesManager.max());
         System.out.println("Усеченное среднее значение из списка продаж равно " + salesManager.statistic());
